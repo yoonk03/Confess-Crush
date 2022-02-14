e94f29938a6d8e25159b274569170ea9966b76b0
yoonk03 <99555763+yoonk03@users.noreply.github.com>
2022-02-14 13:54:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "Valentine",
+    introTitle: 'Лиза',
+    introDesc: `Мы с тобой прошли не малый путь,
+    ты подарила мне счастье и веру в любовь. 
+    Спасибо 😘 `,
+    btnIntro: '♥️',
+    title: 'Отдай все свои деньги 🥰',
+    desc: 'Ну попробуй только нажать нет ',
+    btnYes: 'Да',
+    btnNo: 'Нет',
+    question: 'любишь меня ?',
+    btnReply: 'отправить ',
+    reply: 'Да, ты мой краш',
+    mess: 'Спасибо🥰.И ты моя любимая😘😘',
+    messDesc: 'Пошли гулять?.',
+    btnAccept: 'В субботу пойдём ',
+    messLink: 'https://instagram.com/zerch.0_o?utm_medium=copy_link' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
